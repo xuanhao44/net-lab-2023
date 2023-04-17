2751bbd34d133a574293c4fa568e0aa6348dd0c2
Xuanhao <shirou-emiya2002@outlook.com>
2023-04-17 19:10:22 +0800

    lab2 eth fin

--- src/ethernet.c
+++ src/ethernet.c
@@ -17,21 +17,28 @@ void ethernet_in(buf_t *buf)
     if (buf->len < sizeof(ether_hdr_t)) {
         return;
     }
-    // 在接收时，也需要将大端序转成小端序存放的数值
-    // #define swap16(x) ((((x)&0xFF) << 8) | (((x) >> 8) & 0xFF)) // 为 16 位数据交换大小端
-    // 如果不需要这个阶段处理的数据，就直接传输；如果不超过 1 字节，也可以直接取出数据。
-    // 在开始的 eth 实验中，ethernet_out 和 ethernet_in 都还未使用这些数据，包括 protocol, mac 在内
-    // 故而不先着急使用大小端转换
 
+    // 先把 mac(源 MAC 地址) 和 protocol 从首部中取出来
     ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
 
+    uint8_t mac[NET_MAC_LEN];
+    uint16_t protocol;
+    memcpy(mac, hdr->src, NET_MAC_LEN);
+    // 在接收时，也需要将大端序转成小端序存放的数值
+    // 对象：多字节数值，在这部分中只有 protocol 是 16 位的 2 字节数值，所以只有它需要转换
+    // #define swap16(x) ((((x)&0xFF) << 8) | (((x) >> 8) & 0xFF)) // 为 16 位数据交换大小端
+    protocol = swap16(hdr->protocol16);
+
     // Step2
-    // 调用 buf_remove_header() 函数移除加以太网包头
+    // 调用 buf_remove_header() 函数移除以太网包头
     buf_remove_header(buf, sizeof(ether_hdr_t));
+    // 有憨批在移除了首部之后还在用引用的对象
+    // 错误示例：net_in(buf, hdr->protocol16, hdr->src);
+    // hdr 取的是 buf->data 的起始地址，而在 buf_remove_header() 后起始地址肯定就变了，hdr 就变为野指针了
 
     // Step3
     // 调用 net_in() 函数向上层传递数据包
-    net_in(buf, hdr->protocol16, hdr->src);
+    net_in(buf, protocol, mac);
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -66,20 +73,16 @@ void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol)
     // Step4
     // 填写源 MAC 地址，即本机的 MAC 地址
     memcpy(hdr->src, net_if_mac, NET_MAC_LEN);
-    // 大写的似乎不能用，用了小写的 net_if_mac
+    // 大写的 NET_IF_MAC 不能用，用小写的 net_if_mac
+    // 大写的是字面量，小写的是地址，memcpy 要填地址
 
     // Step5
     // 填写协议类型 protocol
-    hdr->protocol16 = protocol;
+    hdr->protocol16 = swap16(protocol); // 同样注意大小端的转换
 
     // Step6
     // 调用驱动层封装好的 driver_send() 发送函数，将添加了以太网包头的数据帧发送到驱动层
     driver_send(buf);
-    // 在发送之前我们需要将小端存储的字节序转换成大端序存储的数值
-    // #define swap16(x) ((((x)&0xFF) << 8) | (((x) >> 8) & 0xFF)) // 为 16 位数据交换大小端
-    // 如果不需要这个阶段"处理"的数据，就直接传输；如果不超过 1 字节，也可以直接取出数据。
-    // 在开始的 eth 实验中，ethernet_out 和 ethernet_in 都还未使用这些数据，包括 protocol, mac 在内
-    // 故而不先着急使用大小端转换
 }
 /**
  * @brief 初始化以太网协议
