b8cb2d220718f86b8f56e45ee5339fdf9cab1ce0
Xuanhao <shirou-emiya2002@outlook.com>
2023-04-29 14:48:32 +0800

    ip_frag_test pass!

--- src/utils.c
+++ src/utils.c
@@ -82,18 +82,17 @@ uint16_t checksum16(uint16_t *data, size_t len)
 
     // Step1
     // 把 data 看成是每 16 个 bit（即 2 个字节）组成一个数，相加。
-    // 注意，16 位加法的结果可能会超过 16 位，因此加法结果需要用 32 位数来保存。
-    uint32_t res32 = 0;
+    uint32_t res32 = 0;              // 注意，16 位加法的结果可能会超过 16 位，因此加法结果需要用 32 位数来保存。
     for (int i = 0; i < len; i += 2) // len 的单位是字节 (8bit)，但是我们步进的单位是 16bit
     {
-        if (i == len - 1) // Step2 如果最后还剩 8 个 bit 值，也要相加这个 8bit 值。
-        {
-            res32 += data[i / 2 + 1] & 0xFF; // 在低 8 位，就是 &0XFF
-        }
-        else
-        {
-            res32 += data[i / 2];
-        }
+        res32 += data[i / 2];
+    }
+
+    // Step2
+    // 如果最后还剩 8 个 bit 值，也要相加这个 8bit 值。
+    if (len % 2 == 1)
+    {
+        res32 += data[len / 2 + 1]; // 在低 8 位，就是 &0XFF，不过不写这些也行
     }
 
     // Step3
